d1399b0b285921f8fccc9b365c6481ba90d983c5
wuyuehua <wuyuehuafor@163.com>
2017-01-29 14:57:55 +0800

    test cookie

--- articles/test.js
+++ articles/test.js
@@ -10,7 +10,49 @@ function add(){
    var v2 = ipt2.value;//获取input的值
    var ans=parseInt(v1)+parseInt(v2);
    document.getElementById("p1").innerHTML = ans;
+	}
 	
-	
+function sort(){
+	var str=document.getElementById('input3').value;
+	var s=str.split(" ");
+	document.getElementById("p2").innerHTML = s.sort();
 }
-	
+
+function getCookie(c_name)
+{
+if (document.cookie.length>0)
+{ 
+c_start=document.cookie.indexOf(c_name + "=")
+if (c_start!=-1)
+{ 
+c_start=c_start + c_name.length+1 
+c_end=document.cookie.indexOf(";",c_start)
+if (c_end==-1) c_end=document.cookie.length
+return unescape(document.cookie.substring(c_start,c_end))
+} 
+}
+return ""
+}
+
+function setCookie(c_name,value,expiredays)
+{
+var exdate=new Date()
+exdate.setDate(exdate.getDate()+expiredays)
+document.cookie=c_name+ "=" +escape(value)+
+((expiredays==null) ? "" : "; expires="+exdate.toGMTString())
+}
+
+function checkCookie()
+{
+username=getCookie('username')
+if (username!=null && username!="")
+  {alert('Welcome again '+username+'!')}
+else 
+  {
+  username=prompt('Please enter your name:',"")
+  if (username!=null && username!="")
+    {
+    setCookie('username',username,365)
+    }
+  }
+}
